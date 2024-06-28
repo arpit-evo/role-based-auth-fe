e13f34294929139a9cf4ace5eb0a8dda07431a63
arpit-evo <arpit.m@evolutioncloud.in>
2024-06-28 18:59:42 +0530

    feat: add user section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,16 +2,28 @@ import { useEffect, useState } from "react";
 import axiosInstance from "../apis/axiosInstance";
 import { MdLogout } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import UserSection from "../components/UserSection";
 
 const HomePage = () => {
   const [user, setUser] = useState({});
   const [role, setRole] = useState("");
   const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
   useEffect(() => {
     const fetchMe = async () => {
       const response = await axiosInstance.get("/api/user/me");
       setRole(response.data.data.role);
       setUser(response.data.data);
+
+      setValue("email", response.data.data.email);
     };
     fetchMe();
   }, []);
@@ -24,8 +36,8 @@ const HomePage = () => {
   };
 
   return (
-    <div className="p-20">
-      <div className="max-w-7xl flex justify-between mx-auto">
+    <div className="p-20 ">
+      <div className="max-w-7xl flex justify-between mx-auto mb-8">
         <div className="text-2xl">
           {role === "user" && "Home Page"}
           {role === "admin" && "Admin Page"}
@@ -38,6 +50,9 @@ const HomePage = () => {
           <p className="text-2xl">Logout</p>
           <MdLogout className="text-[32px] cursor-pointer" />
         </div>
+      </div>
+      <div className="w-[40rem] mx-auto ">
+        <UserSection user={user} />
       </div>
     </div>
   );
